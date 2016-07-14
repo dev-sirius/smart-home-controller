615bc2ed043729dd3fb5c841dba3b2129e11f8c9
AndreyChertckov <andreychertckov@yandex.ru>
2016-07-14 08:56:57 +0300

    some changes in connecter

--- mysite/Connecter.py
+++ mysite/Connecter.py
@@ -4,14 +4,12 @@ import random
 
 transporter = TransportProtocol()
 
-def connectNewArduino(name, conn_type,ip=0, bluetooth_name=0, chanale = 0):
+def connectNewArduino(name, conn_type,p,g,Bkey,ip=0, bluetooth_name=0, chanale = 0):
     private_key = random.randint(0,10**10)
-    p = random.randint(0,10**10)
-    g = random.randint(0,10**10)
-    transporter.send(p,0,'WIFI')
-    transporter.send(g, 0, 'WIFI')
+
+    transporter.send(p,0,conn_type)
+    transporter.send(g, 0, conn_type)
     Akey = int((g**private_key) % p)
-    transporter.send(Akey,0,'WIFI')
-    Bkey = transporter.getPackageWIFI()
+    transporter.send(Akey,0,conn_type)
     crypto_key = (Bkey**private_key)%p
-    ard = Arduino(name,crypto_key,conn_type,ip,bluetooth_name,chanale)
+    Arduino.objects.create(name,crypto_key,conn_type,ip,bluetooth_name,chanale)
--- smarthouse/models.py
+++ smarthouse/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 
+
 class Arduino(models.Model):
-    name = models.CharField(max_length =50)
-    crypto_key = models.CharField(max_length = 40)
+    name = models.CharField(max_length=50)
+    crypto_key = models.CharField(max_length=40)
     TYPE_CONNECTION = (
-        ('bl','BLUETOOTH'),
-        ('wf','WIFI'),
-        ('rd','RADIO_433')
+        ('bl', 'BLUETOOTH'),
+        ('wf', 'WIFI'),
+        ('rd', 'RADIO_433')
     )
-    conn_type = models.CharField(max_length = 2,
-    choices =  TYPE_CONNECTION,
-    default = "wf")
+    conn_type = models.CharField(max_length=2,
+                                 choices=TYPE_CONNECTION,
+                                 default="wf")
 
     ip = models.GenericIPAddressField()
     port = models.IntegerField()
 
-    name_bluetooth = models.CharField(max_length = 20)
+    name_bluetooth = models.CharField(max_length=20)
     channel = models.IntegerField()
