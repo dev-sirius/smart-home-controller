6fb74356ae652d2d089810c7adc23128cb75f384
AndreyChertckov <andreychertckov@yandex.ru>
2016-07-14 07:58:14 -0400

    some changes

--- mysite/views.py
+++ mysite/views.py
@@ -4,22 +4,22 @@ import os
 
 
 def temp(request):
-	try:
-		temp = request.GET['temperature']
-		return render_to_response('temp.html', {'temperature': temp})
-	except KeyError:
-		resp = HttpResponse()
-		resp.status_code = 406
-		resp.write('Required argument not found.')
-		return resp
+    try:
+        temp = request.GET['temperature']
+        return render_to_response('temp.html', {'temperature': temp})
+    except KeyError:
+        resp = HttpResponse()
+        resp.status_code = 406
+        resp.write('Required argument not found.')
+        return resp
+
 
 def index(request):
-	return render_to_response('index.html', {})
+    return render_to_response('index.html', {})
+
 
 def API(request):
-	if request.method == 'GET':
-		pass
-		#do()
-	elif request.method == 'POST':
-		pass
-		#do2()
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
