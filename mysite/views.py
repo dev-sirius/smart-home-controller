0d9dec69ceff2dde2f79af079dcd8e35019a7949
AndreyChertckov <andreychertckov@yandex.ru>
2016-07-14 11:12:08 -0400

    asds

--- mysite/views.py
+++ mysite/views.py
@@ -29,6 +29,6 @@ def apiUpdate(request):
         pass
     elif request.method == 'POST':
         #jsonPost = request.POST['json']
-        jsonPost = str(request.POST.get())
+        jsonPost = str(request.POST.get('json'))
         return HttpResponse(jsonPost)
     return HttpResponse('<h1>We need a request</h1>')
